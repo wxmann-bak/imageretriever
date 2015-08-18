c5b3e14a0aa0325d8311a913e9a3f81ab2acada6
wxmann <wxfreakk91@gmail.com>
2015-08-18 00:14:17 -0700

    Add a class to handle multiple jobs (use case: saving images for multiple typhoons at different time intervals)

---
+++ src/main/java/com/jimtang/saver/MultipleJobHandler.java
@@ -0,0 +1,43 @@
+package com.jimtang.saver;
+
+import com.jimtang.saver.imagesav.ImageSaver;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Logger;
+
+/**
+ * Created by tangz on 8/17/2015.
+ */
+public class MultipleJobHandler {
+
+    private static final Logger LOGGER = Logger.getLogger(MultipleJobHandler.class.getName());
+
+    private Collection<ImageSaver> savers;
+
+    public MultipleJobHandler() {
+        savers = new ArrayList<>();
+    }
+
+    public void addJob(ImageSaver saver) {
+        savers.add(saver);
+    }
+
+    public void runAll() {
+        if (savers.isEmpty()) {
+            LOGGER.info("No image savers in this job handler. Exit.");
+        }
+        ExecutorService executorService = Executors.newFixedThreadPool(savers.size());
+        for (ImageSaver saver: savers) {
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    saver.execute();
+                }
+            });
+        }
+        executorService.shutdown();
+    }
+}
